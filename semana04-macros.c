6170df2f9ea1e1f238ecd16b9148e1a2fb10646b
Luciane Nagay Martins <36605473+lunanie@users.noreply.github.com>
2019-08-27 21:56:39 -0300

    Update semana04-macros.c

--- semana04-macros.c
+++ semana04-macros.c
@@ -348,4 +348,30 @@ int main(void) {
 
 
 
+#include <stdio.h>
+#include <conio.h>
+#include <time.h>
+
+void sequencia(void) {
+  static int n=0;  
+  printf("%d,",n++);
+}
+
+int main(void) {
+   while( !_kbhit() ) {
+      sequencia();
+      _sleep(1);
+   }
+   return 0;
+}
+
+
+
+THEHUXLEY.COM
+M3BE49
+
+
+http://thehuxley.com/group/fatecsp-ads-n-ilp010?page=1
+
+
 
